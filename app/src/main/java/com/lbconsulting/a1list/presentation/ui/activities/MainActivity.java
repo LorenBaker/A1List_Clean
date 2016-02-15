126240fc7ccc3679745fa091aad6463b52265190
LorenBaker <LorenABaker@comcast.net>
2016-02-15 14:52:34 -0800

    Added MainActivity normal Overrides

--- app/src/main/java/com/lbconsulting/a1list/presentation/ui/activities/MainActivity.java
+++ app/src/main/java/com/lbconsulting/a1list/presentation/ui/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.lbconsulting.a1list.presentation.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.lbconsulting.a1list.R;
 import com.lbconsulting.a1list.presentation.presenters.MainPresenter.View;
@@ -27,5 +29,35 @@ public class MainActivity extends AppCompatActivity implements View {
     @Override
     public void showError(String message) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
